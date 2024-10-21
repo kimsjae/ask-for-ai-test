dc587961acbc05d9f436bc02e6bad0a95e310320
kimseongjae <stwin755@gmail.com>
2024-10-21 19:11:49 +0900

    first commit

---
+++ src/main/java/org/askforai/AskForAiApplication.java
@@ -0,0 +1,13 @@
+package org.askforai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskForAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskForAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/askforai/AskForAiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.askforai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskForAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
